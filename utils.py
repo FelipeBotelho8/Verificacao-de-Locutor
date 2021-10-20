e6cb224a0aa76817ebe233a5a3ec25d70432f4d4
Felipe Silva <felibnunes@hotmail.com>
2021-10-20 10:07:36 -0300

    Modificação de parâmetros, alteração da rede neural, separação do treino do teste e mo

--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 from config import *
-import numpy as np
 import librosa
 
 def get_mfcc_coefs(path):
@@ -12,9 +11,9 @@ def get_mfcc_coefs(path):
     
     audio, sr = librosa.load(path, sr=SAMPLE_RATE, duration=DURATION)
     audio_emp = librosa.effects.preemphasis(audio)
-    coefs = librosa.feature.mfcc(y=audio_emp, sr=SAMPLE_RATE, n_mfcc=MEL_ORDER, n_fft=FRAME_LENGTH, hop_length=160, n_mels='128') 
-    if coefs.shape[1] != 300:
-        coefs = librosa.util.fix_length(coefs, 300, axis=1)
+    coefs = librosa.feature.mfcc(y=audio_emp, sr=SAMPLE_RATE, n_mfcc=MEL_ORDER, n_fft=FRAME_LENGTH, hop_length=HOP_LENGTH, n_mels=N_MELS) 
+    if coefs.shape[1] != NUM_FRAMES:
+        coefs = librosa.util.fix_length(coefs, NUM_FRAMES, axis=1)
     coefs = coefs.reshape(-1)
 
     return coefs
